d253d0b8b43e818bd0514e6bfaf8bf882a4fc562
GabPanza <gabrieljp@id.uff.br>
2022-06-20 22:18:45 -0300

    Update 2231-Temperatura_Lunar.py

--- 2231-Temperatura_Lunar.py
+++ 2231-Temperatura_Lunar.py
@@ -1,14 +1,17 @@
-def preencheTemp(temperatura):
+def preencheTemp(N):
+    temperatura = []
     for k in range(N):
         temperatura.append(int(input()))
     return temperatura
 
-def calculaSomasTemp(temperatura):
+def calculaSomasTemp(N,M,temperatura):
     somas = []
-    for i in range(N-(M-1)):
-        soma=0
-        for j in range(i, (i+M)):
-            soma += temperatura[j]
+    soma=0
+    for j in range(M):
+        soma += temperatura[j]
+    somas.append(soma)
+    for i in range(N-M):
+        soma += temperatura[i+M] - temperatura[i]
         somas.append(soma)
     return somas
 
@@ -20,20 +23,26 @@ def calcMaiorMenor(somas, maior, menor):
             menor = i
     return maior,menor
 
-cont=0
-N, M = list(map(int, input(). split()))
+lista_menores_maiores = []
+N, M = map(int, input().split())
 while N!=0 and M!=0:
-    temperatura = []
     maior= -201
     menor = 201
     
     # Chamo minhas funçoes
-    temperatura = preencheTemp(temperatura)
-    somas = calculaSomasTemp(temperatura)
+    temperatura = preencheTemp(N)
+    somas = calculaSomasTemp(N,M,temperatura)
     maior,menor = calcMaiorMenor(somas,maior,menor)
     
-    # Imprimo os testes
-    cont += 1
-    print ("Teste",cont)
-    print(int(menor/M), ' ', int(maior/M), '\n')
-    N, M = list(map(int, input(). split()))
+    # Salvo os maiores e menores
+    lista_menores_maiores.append(int(menor/M))
+    lista_menores_maiores.append(int(maior/M))
+    
+    # Reseto o loop
+    N, M = map(int, input().split())
+cont=1
+for i in range(0,len(lista_menores_maiores),2):
+    print("Teste", cont)
+    print(lista_menores_maiores[i], lista_menores_maiores[i+1])
+    print()
+    cont+=1
