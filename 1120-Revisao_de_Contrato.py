dac221e2c64ac581fcd41c5f9ac74626c4953fdb
GabPanza <gabrieljp@id.uff.br>
2022-06-25 11:34:17 -0300

    Update

--- 1120-Revisao_de_Contrato.py
+++ 1120-Revisao_de_Contrato.py
@@ -1,21 +1,15 @@
 import collections
+
 erro, valor = map(int, input().split())
 lista_vazia = []
-
 while erro!= 0 and valor != 0:
     erro = str(erro)
     valor = str(valor)
     for i in range(len(str(erro))):
         valor = valor.replace(erro[i],"")
-    
-    lista_com_zeros = ["0"] * len(str(valor))
-    if collections.Counter(str(valor)) == collections.Counter(lista_vazia):
-        print(0)
-    
-    elif collections.Counter(str(valor)) == collections.Counter(lista_com_zeros):
-        print(0)
-    
-    elif collections.Counter(str(valor)) != collections.Counter(lista_vazia) and collections.Counter(str(valor)) != collections.Counter(lista_com_zeros):
+    lista_com_zeros = ["0"] * len(valor)
+    if collections.Counter(str(valor)) != collections.Counter(lista_vazia) and collections.Counter(str(valor)) != collections.Counter(lista_com_zeros):
         print(int(valor))
-    
+    else:
+        print(0)
     erro, valor = map(int, input().split())
--- 2466-Sinuca.py
+++ 2466-Sinuca.py
@@ -1,21 +1,17 @@
 qntd_de_bolas_inicial = int(input())
-cores = list(map(int, input().split()))
-fileiras = []
-
-for i in cores:
-    fileiras.append(i)
+linha_de_bolas = list(map(int, input().split()))
 
 while qntd_de_bolas_inicial > 1:
-    fileiras_seguintes = []
+    linha_de_bolas_seguintes = []
     for i in range(qntd_de_bolas_inicial - 1):
-        if fileiras[i] == fileiras[i+1]:
-            fileiras_seguintes.append(1)
+        if linha_de_bolas[i] == linha_de_bolas[i+1]:
+            linha_de_bolas_seguintes.append(1)
         else:
-            fileiras_seguintes.append(-1)
-    fileiras = fileiras_seguintes
+            linha_de_bolas_seguintes.append(-1)
+    linha_de_bolas = linha_de_bolas_seguintes
     qntd_de_bolas_inicial -= 1
 
-if fileiras[0] == 1:
+if linha_de_bolas[0] == 1:
     print("preta")
-elif fileiras[0] == -1:
+elif linha_de_bolas[0] == -1:
     print("branca")
