7fbebb4b7ea5800208c8fd1ca92e24958d7cc3eb
GabPanza <gabrieljp@id.uff.br>
2022-06-27 18:19:48 -0300

    Update

---
+++ 2415-Consecutivos.py
@@ -0,0 +1,12 @@
+N_qntd_numeros = int(input())
+N_numeros = list(map(int, input().split()))
+maiorSequencia=[]
+cont=1
+for i in range(N_qntd_numeros-1):
+    if N_numeros[i]==N_numeros[i+1]:
+        cont+=1
+    else:
+        maiorSequencia.append(cont)
+        cont=1
+maiorSequencia.sort()
+print(maiorSequencia[-1])
