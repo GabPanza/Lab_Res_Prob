f52ed5cc3575562890309ed15171b8270c80a2fd
GabPanza <gabrieljp@id.uff.br>
2022-06-28 13:39:39 -0300

    Update 2301-Vivo_ou_Morto.py

--- 2301-Vivo_ou_Morto.py
+++ 2301-Vivo_ou_Morto.py
@@ -1,6 +1,5 @@
 participantes, rodadas = map(int, input().split())
-vencedores = []
-
+cont=1 
 while participantes!=0 or rodadas != 0:
     fila_inicial = list(map(int, input().split()))
     for i in range(rodadas):
@@ -8,15 +7,12 @@ while participantes!=0 or rodadas != 0:
         participantesRestantes = rodada[2:]
         while 0 in fila_inicial:
             fila_inicial.remove(0)
-        for j in range(len(rodada)):
+        for j in range(len(participantesRestantes)):
             if participantesRestantes[j] != rodada[1]:
                 fila_inicial[j] = 0           
-        vencedor = max(fila_inicial)
-        vencedores.append(vencedor)
-        participantes, rodadas = map(int, input().split())
-cont=1        
-for i in range(len(vencedores)):
+    vencedor = max(fila_inicial)
     print("Teste {}".format(cont))
-    print(vencedores[i])
+    print(vencedor)
     print()
     cont+=1
+    participantes, rodadas = map(int, input().split())
